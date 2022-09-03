f1a8adbc2906abd6bc102428fc30cbf9f5ebdb91
Alik <9774952689@bk.ru>
2022-09-04 01:55:47 +0300

    CompletedIсeCream

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число шариков которое вы бы хотели приобрести: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a==3 || a==5)
+{
+    Console.WriteLine("Да вы можете приобрести " +a + " шариков(а)");
+}
+else
+{
+    Console.WriteLine("К сожалению "+a +" шариков(а) приобрести нельзя");
+}
